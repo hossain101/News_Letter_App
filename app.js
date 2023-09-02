31a63b2a563dfc5bcee9a3808fe194992b1f830f
shoaib <shoaibhossain24@gmail.com>
2023-09-03 04:14:32 +0600

    up until setting up .env file

--- app.js
+++ app.js
@@ -1,29 +1,94 @@
-const express  = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+require("dotenv").config();
+//console.log(process.env.API_TOKEN);
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
-const app  = express();
-
-app.get('',(_req,_res)=>{
-    _res.send('Hello Express');
+app.get("/", (_req, _res) => {
+  _res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", (_req, _res) => {
+  var firstName = _req.body.firstName;
+  var lastName = _req.body.lastName;
+  var email = _req.body.email;
 
+  //   const client = require("@mailchimp/mailchimp_marketing");
 
+  //   client.setConfig({
+  //     apiKey: "9389d57e853038a3b63feb207c5bf3d8-us14",
+  //     server: "us14",
+  //   });
 
+  //   const run = async () => {
+  //     const response = await client.lists.addListMember("0339d06b88", {
+  //       email_address: email,
+  //       status: "subscribed",
+  //       merge_fields: {
+  //         FNAME: firstName,
+  //         LNAME: lastName,
+  //       },
+  //     });
+  //     console.log(response);
+  //   };
 
+  //   run();
 
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
 
+  const jsonData = JSON.stringify(data);
 
+  const url = "https://us14.api.mailchimp.com/3.0/lists/0339d06b88";
 
+  const options = {
+    method: "POST",
+    auth: `auth:${process.env.API_TOKEN}`,
+  };
 
+  const request = https.request(url, options, (response) => {
+    try {
+      if (!process.env.API_TOKEN) {
+        throw new Error("API_TOKEN not found");
+      }
 
+      if (response.statusCode === 200) {
+        _res.sendFile(__dirname + "/success.html");
+      } else {
+        _res.sendFile(__dirname + "/failure.html");
+      }
 
+      response.on("data", (data) => {
+        console.log(JSON.parse(data));
+      });
+    } catch (err) {
+      console.error(err);
+      _res.send(err.message);
+    }
+  });
 
-
-
-
-
-
-
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+  request.write(jsonData);
+  request.end();
 });
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+//67fc9f8cef48e6a17400a2c2f2ca5528-us14
+
+//0339d06b88
